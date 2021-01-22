abd8ce90a1a01beba7c4a4556639ea72a9ca2fd1
rawshad <rawshad@outlook.com>
2021-01-22 19:35:39 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,46 @@
+// Kilometer to meter Converter
+function kilometerToMeter(num) {
+    var meter = num * 1000;
+    return meter;
+}
+
+var result = kilometerToMeter(7);
+
+console.log("7 kilometer =", result, "meter");
+
+
+//Budget Calculator
+function budgetCalculator(watch, phone, laptop) {
+    var result = watch * 50 + phone * 100 + laptop * 500;
+    return result;
+}
+
+var budget = budgetCalculator(5, 10, 7);
+console.log('Total Budget =', budget);
+
+function hotelCost(day) {
+    var cost = 0;
+    if (day <= 10) {
+        cost = day * 100;
+        return cost;
+    }
+    else if (day <= 20) {
+        var tenDayPrice = 10 * 100;
+        var remaining = day - 10;
+        var secondTenDayPrice = remaining * 80;
+        cost = tenDayPrice + secondTenDayPrice;
+        return cost;
+    }
+    else {
+        var tenDayPrice = 10 * 100;
+        var secondTenDayPrice = 10 * 80;
+        var remaining = day - 20;
+        var remainingDayPrice = remaining * 50;
+        cost = tenDayPrice + secondTenDayPrice + remainingDayPrice;
+        return cost;
+    }
+}
+
+var totalCost = hotelCost(12);
+
+console.log('Total Hotel Cost =', totalCost);
